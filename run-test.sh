8960d23301dd0056ff579679fae5590d3a964a1a
Ben Lau <xbenlau@gmail.com>
2017-10-14 21:23:23 +0800

    Fix test case

---
+++ run-test.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+rm -rf build
+mkdir build
+cd build
+QT_CI_SILENT=true run-unittests $2
+
+if [ $? -eq 0 ]; then
+    node logger/add.js "$1" pass
+else
+    node logger/add.js "$1" fail
+fi
+
