151442d72db595ef1627fd0fdbcd9d623c49d1d0
Ben Lau <xbenlau@gmail.com>
2017-10-14 21:59:41 +0800

    Fix test case

--- logger/create.js
+++ logger/create.js
@@ -4,7 +4,7 @@ var xml2js = require('xml2js');
 
 var input = argv._[0];
 var name = argv._[1];
-var package = argv._[2];
+var packageName = argv._[2];
 var content  = shell.cat(input).toString();
 
 var parser = new xml2js.Parser();
@@ -16,7 +16,14 @@ parser.parseString(content, function (err, result) {
 
 var packages = data.Packages.Package;
 
-var index = packages.findIndex(function(o) { return o.Name[0] === package});
+var index = packages.findIndex(function(o) { return o.Name[0] === packageName});
+
+if (index < 0) {
+    console.err(packageName, "not found");
+    console.log(JSON.stringify(packages,null,4));
+    return -1;
+}
+
 var package = packages[index];
 
 var output = {
@@ -26,3 +33,4 @@ var output = {
 }
 
 shell.ShellString(JSON.stringify(output,null,4)).to("result.json");
+
