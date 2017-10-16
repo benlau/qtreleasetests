bd17374f2e46c86324d8069def133163584f231b
Ben Lau <xbenlau@gmail.com>
2017-10-16 22:09:06 +0800

    Add QTBUG-49370

--- qtbug-49370/main.cpp
+++ qtbug-49370/main.cpp
@@ -1,9 +1,30 @@
 #include <QtQuickTest/quicktest.h>
 #include <QtGlobal>
+#include <QtConcurrent>
+#include <unistd.h>
+#include <signal.h>
 
 int main(int argc, char **argv)
 {
     qputenv("QTEST_FUNCTION_TIMEOUT", "1000");
 
-    return quick_test_main(argc, argv, "QuickTests", QUICK_TEST_SOURCE_DIR);
+    bool running = true;
+
+    QThreadPool pool;
+    QFuture<void> future = QtConcurrent::run(&pool, [&]() {
+        qDebug() << "Start watchdog";
+        sleep(3);
+        if (running) {
+            qDebug() << "Timeout. Watchdog kill current process";
+            kill(getpid(), SIGKILL);
+            return;
+        }
+        qDebug() << "Watchdog quit";
+    });
+
+    quick_test_main(argc, argv, "QuickTests", QUICK_TEST_SOURCE_DIR);
+    running = false;
+    future.waitForFinished();
+
+    return 0;
 }
